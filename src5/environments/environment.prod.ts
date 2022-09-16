ac4721103ee621b155406f6cb5062332a950e9c8
felipezzz1 <80336800+felipezzz1@users.noreply.github.com>
2022-09-16 11:11:33 -0300

    aula do dia 16/09

---
+++ src5/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCCzyLQ012hlhbRchkxtgRUYLlFW9yZJfM",
+    authDomain: "appcontatos-af36b.firebaseapp.com",
+    projectId: "appcontatos-af36b",
+    storageBucket: "appcontatos-af36b.appspot.com",
+    messagingSenderId: "816101393453",
+    appId: "1:816101393453:web:8b4ca1d01fd908c03e24a4"
+  }
+};
